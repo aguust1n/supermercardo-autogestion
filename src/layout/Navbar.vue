8548ff6fc4a78dbc2e424e4fa217560f5ad12941
aguust1n <aguztin48@gmail.com>
2021-09-12 13:13:26 -0300

    #5 Add navbar

---
+++ src/layout/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-app-bar color="blue-grey darken-4" dense>
+        <router-link to="/" class="hidden-md-and-up">
+            <v-img
+                src="/img/logoSuper_blanco.svg"
+                height="100"
+                width="180"
+            />
+        </router-link>
+        <router-link to="/">
+            <v-img
+                class="mx-2 hidden-sm-and-down"
+                src="/img/logoSuper_blanco.svg"
+                width="200"
+            />
+        </router-link>
+        <v-icon>mdi-login-variant</v-icon>
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
